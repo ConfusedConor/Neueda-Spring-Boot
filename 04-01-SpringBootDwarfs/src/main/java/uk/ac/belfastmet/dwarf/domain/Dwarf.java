9c506f8b919d9d15def4c31effd1665883e0b1a3
MCE18161675 <MCE18161675@CLCLRCAC17.belfastmet.int>
2018-11-21 09:38:59 +0000

    Initial Dwarf upload for learning activity

---
+++ 04-01-SpringBootDwarfs/src/main/java/uk/ac/belfastmet/dwarf/domain/Dwarf.java
@@ -0,0 +1,17 @@
+package uk.ac.belfastmet.dwarf.domain;
+
+public class Dwarf {
+public Dwarf(String name, String author, String image) {
+
+		this.name = name;
+		this.author = author;
+		this.image = image;
+	}
+public Dwarf() {
+		super();
+	}
+private String name;
+private String author;
+private String image;
+
+}
