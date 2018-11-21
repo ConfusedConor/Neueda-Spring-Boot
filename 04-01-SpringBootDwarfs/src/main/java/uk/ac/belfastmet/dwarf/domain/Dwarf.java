9e9f92946d6cc689a880931fcb98b7ff47eb4dc5
MCE18161675 <MCE18161675@CLCLRCAC17.belfastmet.int>
2018-11-21 09:59:51 +0000

    Dwarfs & Dwarves added in

--- 04-01-SpringBootDwarfs/src/main/java/uk/ac/belfastmet/dwarf/domain/Dwarf.java
+++ 04-01-SpringBootDwarfs/src/main/java/uk/ac/belfastmet/dwarf/domain/Dwarf.java
@@ -1,6 +1,24 @@
 package uk.ac.belfastmet.dwarf.domain;
 
 public class Dwarf {
+public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
 public Dwarf(String name, String author, String image) {
 
 		this.name = name;
