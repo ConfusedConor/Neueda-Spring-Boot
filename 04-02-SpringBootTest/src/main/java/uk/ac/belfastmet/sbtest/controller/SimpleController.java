5714cc2d1dc5f3d39bc9370b8b3caad8c9bb2794
ubuntu <ubuntu@NeuedaUbuntu-ConorM>
2018-11-22 16:19:50 +0000

    Some updated styling

--- 04-02-SpringBootTest/src/main/java/uk/ac/belfastmet/sbtest/controller/SimpleController.java
+++ 04-02-SpringBootTest/src/main/java/uk/ac/belfastmet/sbtest/controller/SimpleController.java
@@ -25,6 +25,7 @@ import uk.ac.belfastmet.sbtest.service.SRService;
 		ArrayList<Rocket> rockets = rocketService.getRockets();
 		model.addAttribute("rockets", rockets);
 		model.addAttribute("pageTitle", "Housten, we have a problem...");
+		model.addAttribute("LocationFrame", "<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224378.83484890635!2d-80.74951446676968!3d28.512075777411198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e74b95a365c6fd%3A0x8b7339f246f869dd!2sKennedy+Space+Center!5e0!3m2!1sen!2suk!4v1542902996444\" width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\" allowfullscreen></iframe>");
 		return "rockets.html";}
 	
 		@GetMapping("/")
