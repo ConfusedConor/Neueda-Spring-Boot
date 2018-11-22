53820834c05fd45b40d2782832677076e7ad973f
ubuntu <ubuntu@NeuedaUbuntu-ConorM>
2018-11-22 15:54:36 +0000

    Shrek & rocket Pages now have basic functionality, showing info

--- 04-02-SpringBootTest/src/main/java/uk/ac/belfastmet/sbtest/controller/SimpleController.java
+++ 04-02-SpringBootTest/src/main/java/uk/ac/belfastmet/sbtest/controller/SimpleController.java
@@ -4,24 +4,33 @@ import org.springframework.stereotype.Controller;
 	import org.springframework.ui.Model;
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.RequestMapping;
+
+import uk.ac.belfastmet.sbtest.domain.Rocket;
+import uk.ac.belfastmet.sbtest.domain.Shrek;
+import uk.ac.belfastmet.sbtest.service.SRService;
 		@Controller
 		@RequestMapping
 	public class SimpleController {
 		@GetMapping("/shrek")
 		public String shrek(Model model) {
-		DwarfService dwarfService = new DwarfService();
-		ArrayList<Dwarf> disneyDwarfs = dwarfService.getDisneyDwarfs();
-		model.addAttribute("disneyDwarfs", disneyDwarfs);
+		SRService shrekService = new SRService();
+		ArrayList<Shrek> shreks= shrekService.getShrekChars();
+		model.addAttribute("shreks", shreks);
 		model.addAttribute("pageTitle", "SOMEBODY ONCE TOLD ME...");
 		return "shrek.html";}
 		
-		@GetMapping("/rocket")
-		public String rocket(Model model) {
-		DwarfService dwarfService = new DwarfService();
-		ArrayList<Dwarf> disneyDwarfs = dwarfService.getDisneyDwarfs();
-		model.addAttribute("disneyDwarfs", disneyDwarfs);
+		@GetMapping("/rockets")
+		public String rockets(Model model) {
+		SRService rocketService = new SRService();
+		ArrayList<Rocket> rockets = rocketService.getRockets();
+		model.addAttribute("rockets", rockets);
 		model.addAttribute("pageTitle", "Housten, we have a problem...");
-		return "rocket.html";}
+		return "rockets.html";}
 	
+		@GetMapping("/")
+		public String home(Model model) {
+			model.addAttribute("pageTitle", "Welcome to the most pointless page not on the internet.");
+			return "index.html";}
+		}
 		
-	}
+	
