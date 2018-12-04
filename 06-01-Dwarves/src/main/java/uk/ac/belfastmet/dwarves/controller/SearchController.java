6687648fd9a3e9e8f19dda555c307ea5159e0852
Conor McErlean (MCE18161675) <MCE18161675@belfastmet.ac.uk>
2018-12-04 13:32:32 +0000

    Updates: Using Searchfields to open individual pages

---
+++ 06-01-Dwarves/src/main/java/uk/ac/belfastmet/dwarves/controller/SearchController.java
@@ -0,0 +1,47 @@
+package uk.ac.belfastmet.dwarves.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import uk.ac.belfastmet.dwarves.repository.DwarfRepository;
+@Controller
+	@RequestMapping("")
+public class SearchController {
+	DwarfRepository dwarfRepository;
+	public SearchController(DwarfRepository dwarfRepository) {
+		super();
+		this.dwarfRepository = dwarfRepository;
+	}
+	
+	@PostMapping("/searchid")
+	public String searchId(@RequestParam("id") Integer dwarfId, Model model)
+	{
+		model.addAttribute("disneyDwarfs", this.dwarfRepository.findByDwarfId(dwarfId));
+		model.addAttribute("pageTitle","Search!");
+		return "dwarfPage";}
+	
+
+@PostMapping("/searchname")
+public String searchName(@RequestParam("name") String name, Model model)
+{
+	model.addAttribute("disneyDwarfs", this.dwarfRepository.findByName(name));
+	model.addAttribute("pageTitle","Search!");
+	return "dwarfPage";}
+
+@PostMapping("/searchauthor")
+public String searchAuthor(@RequestParam("author") String author, Model model)
+{
+	model.addAttribute("disneyDwarfs", this.dwarfRepository.findByAuthor(author));
+	model.addAttribute("pageTitle","Search!");
+	return "dwarfPage";}
+@PostMapping("/searchnameauthor")
+public String searchNameAuthor(@RequestParam("name") String name, @RequestParam("author") String author, Model model)
+{
+	model.addAttribute("disneyDwarfs", this.dwarfRepository.findByNameAndAuthor(name, author));
+	model.addAttribute("pageTitle","Search!");
+	return "dwarfPage";}
+}
--- 06-01-Dwarves/src/main/java/uk/ac/belfastmet/dwarves/repository/DwarfRepository.java
+++ 06-01-Dwarves/src/main/java/uk/ac/belfastmet/dwarves/repository/DwarfRepository.java
@@ -6,4 +6,10 @@ import uk.ac.belfastmet.dwarves.domain.Dwarf;
 
 public interface DwarfRepository extends CrudRepository<Dwarf, Integer> {
 Iterable<Dwarf>findByAuthor(String author);
+
+Dwarf findByDwarfId(Integer dwarfId);
+
+Object findByName(String name);
+
+Object findByNameAndAuthor(String name, String author);
 }
