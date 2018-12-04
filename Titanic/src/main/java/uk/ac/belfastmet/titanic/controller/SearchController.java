86a7d2cf9f6fe83675044848f8c1c4d7dbf185d0
Conor McErlean (MCE18161675) <MCE18161675@belfastmet.ac.uk>
2018-12-04 14:27:34 +0000

    Titanic Bootstrap Project In Progress

---
+++ Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/SearchController.java
@@ -0,0 +1,12 @@
+package uk.ac.belfastmet.titanic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+@RequestMapping("")
+public class SearchController {
+	
+}
---
+++ Titanic/src/main/java/uk/ac/belfastmet/titanic/domain/Passenger.java
@@ -0,0 +1,139 @@
+package uk.ac.belfastmet.titanic.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Passenger {
+	@Id
+private Integer passengerId;
+private boolean survived;
+private Integer pclass;
+private String name;
+private String sex;
+private Integer age;
+private Integer sibSp;
+private Integer parch;
+private String ticket;
+private String fare;
+private String cabin;
+private String embarked;
+
+public Passenger() {
+	super();
+}
+
+public Passenger(Integer passengerId, boolean survived, Integer pclass, String name, String sex, Integer age,
+		Integer sibSp, Integer parch, String ticket, String fare, String cabin, String embarked) {
+	super();
+	this.passengerId = passengerId;
+	this.survived = survived;
+	this.pclass = pclass;
+	this.name = name;
+	this.sex = sex;
+	this.age = age;
+	this.sibSp = sibSp;
+	this.parch = parch;
+	this.ticket = ticket;
+	this.fare = fare;
+	this.cabin = cabin;
+	this.embarked = embarked;
+}
+
+public Integer getPassengerId() {
+	return passengerId;
+}
+
+public void setPassengerId(Integer passengerId) {
+	this.passengerId = passengerId;
+}
+
+public boolean isSurvived() {
+	return survived;
+}
+
+public void setSurvived(boolean survived) {
+	this.survived = survived;
+}
+
+public Integer getPclass() {
+	return pclass;
+}
+
+public void setPclass(Integer pclass) {
+	this.pclass = pclass;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getSex() {
+	return sex;
+}
+
+public void setSex(String sex) {
+	this.sex = sex;
+}
+
+public Integer getAge() {
+	return age;
+}
+
+public void setAge(Integer age) {
+	this.age = age;
+}
+
+public Integer getSibSp() {
+	return sibSp;
+}
+
+public void setSibSp(Integer sibSp) {
+	this.sibSp = sibSp;
+}
+
+public Integer getParch() {
+	return parch;
+}
+
+public void setParch(Integer parch) {
+	this.parch = parch;
+}
+
+public String getTicket() {
+	return ticket;
+}
+
+public void setTicket(String ticket) {
+	this.ticket = ticket;
+}
+
+public String getFare() {
+	return fare;
+}
+
+public void setFare(String fare) {
+	this.fare = fare;
+}
+
+public String getCabin() {
+	return cabin;
+}
+
+public void setCabin(String cabin) {
+	this.cabin = cabin;
+}
+
+public String getEmbarked() {
+	return embarked;
+}
+
+public void setEmbarked(String embarked) {
+	this.embarked = embarked;
+}
+
+}
