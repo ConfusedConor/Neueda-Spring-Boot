d52b4a01568c99f3a2647026ca4848e5fa41f4fb
Conor McErlean (MCE18161675) <MCE18161675@belfastmet.ac.uk>
2018-12-04 15:21:41 +0000

    Update: All Passengers Page Now Functional

--- Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/SearchController.java
+++ Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/SearchController.java
@@ -2,11 +2,25 @@ package uk.ac.belfastmet.titanic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import uk.ac.belfastmet.titanic.repository.PassengerRepository;
 @Controller
 @RequestMapping("")
 public class SearchController {
+	PassengerRepository passengerRepository;
+	public SearchController(PassengerRepository passengerRepository) {
+		super();
+		this.passengerRepository = passengerRepository;
+	}
+@GetMapping("/all")
 	
+	
+	public String all(Model model) {
+		model.addAttribute("passenger", this.passengerRepository.findAll());
+		model.addAttribute("pageTitle", "All Passengers Aboard the H.M.S. Titanic");
+		return "passengerPage";
+}
 }
