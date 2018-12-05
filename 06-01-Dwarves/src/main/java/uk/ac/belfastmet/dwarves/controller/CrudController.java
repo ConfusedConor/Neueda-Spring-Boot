488c8dd5284dbcd391cad837d453d2e3113374b7
MCE18161675 <mce18161675@belfastmet.ac.uk>
2018-12-05 14:49:51 +0000

    Update: Mostly spelling updates and some testing. Also database corrected after using the delete function.

--- 06-01-Dwarves/src/main/java/uk/ac/belfastmet/dwarves/controller/CrudController.java
+++ 06-01-Dwarves/src/main/java/uk/ac/belfastmet/dwarves/controller/CrudController.java
@@ -18,18 +18,24 @@ public class CrudController {
 		super();
 		this.dwarfRepository = dwarfRepository;
 	}
+	
+	//View an individual dwarf
 	@GetMapping("/view/{dwarfId}")
 	public String viewDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model)
 	{
 		model.addAttribute("dwarf", this.dwarfRepository.findByDwarfId(dwarfId));
 		model.addAttribute("pageTitle","Viewing Dwarfs");
 		return "viewDwarf";}
+	
+	//Delete Function
 	@GetMapping("/delete/{dwarfId}")
 	public String deleteDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model, RedirectAttributes redirectattributes)
 	{this.dwarfRepository.deleteById(dwarfId);
-	redirectattributes.addFlashAttribute("message", "Dwarf has been Thanos CLicked");
+	redirectattributes.addFlashAttribute("message", "Dwarf has been Thanos Clicked");
 	return "redirect:/all";
 	}
+	
+	//To be done at later date.
 	@GetMapping("/edit/{dwarfId}")
 	
 	public String editDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model)
@@ -37,4 +43,20 @@ public class CrudController {
 		model.addAttribute("dwarf", this.dwarfRepository.findByDwarfId(dwarfId));
 		model.addAttribute("pageTitle","Viewing Dwarfs");
 		return "viewDwarf";}
+	
+	//Pay no attention to this...
+	@GetMapping("/click")
+	public String click(Integer dwarfId, Model model, RedirectAttributes redirectattributes)
+	{this.dwarfRepository.deleteById(4);
+	this.dwarfRepository.deleteById(2);
+	this.dwarfRepository.deleteById(7);
+	this.dwarfRepository.deleteById(8);
+	this.dwarfRepository.deleteById(9);
+	this.dwarfRepository.deleteById(10);
+	this.dwarfRepository.deleteById(14);
+	this.dwarfRepository.deleteById(16);
+	this.dwarfRepository.deleteById(17);
+	redirectattributes.addFlashAttribute("message", "Half the Dwarfs no longer exist.");
+	return "redirect:/all";
+	}
 }
