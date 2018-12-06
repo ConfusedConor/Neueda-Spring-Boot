fbfaa4b0d47ded8359962687278147aa3df10418
MCE18161675 <mce18161675@belfastmet.ac.uk>
2018-12-06 11:56:45 +0000

    Update: Now includes add, edit and save functions

--- 06-01-Dwarves/src/main/java/uk/ac/belfastmet/dwarves/controller/CrudController.java
+++ 06-01-Dwarves/src/main/java/uk/ac/belfastmet/dwarves/controller/CrudController.java
@@ -1,12 +1,17 @@
 package uk.ac.belfastmet.dwarves.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import uk.ac.belfastmet.dwarves.domain.Dwarf;
 import uk.ac.belfastmet.dwarves.repository.DwarfRepository;
 
 @Controller
@@ -35,14 +40,38 @@ public class CrudController {
 	return "redirect:/all";
 	}
 	
-	//To be done at later date.
+	//Edit Function
 	@GetMapping("/edit/{dwarfId}")
 	
 	public String editDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model)
 	{
 		model.addAttribute("dwarf", this.dwarfRepository.findByDwarfId(dwarfId));
-		model.addAttribute("pageTitle","Viewing Dwarfs");
-		return "viewDwarf";}
+		model.addAttribute("pageTitle","Editing Dwarfs");
+		return "editDwarf";}
+	
+	//Add Function
+		@GetMapping("/add/")
+		
+		public String addDwarf(Model model)
+		{
+			model.addAttribute("pageTitle", "add");
+			model.addAttribute("dwarf", new Dwarf());
+			return "editDwarf";}
+		
+	//Save Function
+		@PostMapping("/save")
+				
+		public String saveDwarf(@Valid Dwarf dwarf,BindingResult bindingResult, Model model)
+		{
+			if(bindingResult.hasErrors()) {
+				return "editDwarf";
+			}
+			else {
+			Dwarf savedDwarf= this.dwarfRepository.save(dwarf);
+			//redirectattributes.addFlashAttribute("message", "New Dwarf Created!");
+			return "redirect:/view/"+dwarf.getDwarfId();
+			}		
+			}
 	
 	//Pay no attention to this...
 	@GetMapping("/click")
