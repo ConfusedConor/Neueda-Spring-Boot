81016bda87b67c3a85bc9171f4af0fcaa8994b19
MCE18161675 <mce18161675@belfastmet.ac.uk>
2018-12-06 16:02:24 +0000

    Update: Cereals by manufacturer added

--- Cereal/src/main/java/uk/ac/belfastmet/cereal/controller/SearchController.java
+++ Cereal/src/main/java/uk/ac/belfastmet/cereal/controller/SearchController.java
@@ -19,14 +19,14 @@ public class SearchController {
 			super();
 			this.cerealRepository = cerealRepository;
 		}
+	//All cereals
 	@GetMapping("/all")
-		
-		
 		public String all(Model model) {
 			model.addAttribute("cereal", this.cerealRepository.findAll());
 			model.addAttribute("pageTitle", "All Cereals");
 			return "cereal";
 	}
+	//Search
 	@PostMapping("/searchCereal")
 	public String searchCereal(@RequestParam("cereal") String cereal, Model model)
 	{
@@ -34,5 +34,69 @@ public class SearchController {
 		model.addAttribute("pageTitle","Search by Cereal!");
 		return "cereal";}
 	
+	//Kellogs
+	@GetMapping("/kellogs")
+	public String kellogs(Model model) {
+		model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Kellogg's"));
+		model.addAttribute("pageTitle", "Kellogg's Cereals");
+		return "cereal";
+	}
+	//Nestle
+		@GetMapping("/nestle")
+		public String nestle(Model model) {
+			model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Nestle"));
+			model.addAttribute("pageTitle", "Nesle's Cereals");
+			return "cereal";
+		}
 
+	//Sainsburys
+		@GetMapping("/sainsburys")
+		public String sainsburys(Model model) {
+			model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Sainsbury's"));
+			model.addAttribute("pageTitle", "Sainsbury's Cereals");
+			return "cereal";
+		}
+	//Weetabix
+		@GetMapping("/weetabix")
+		public String weetabix(Model model) {
+			model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Weetabix"));
+			model.addAttribute("pageTitle", "Weetabix's Cereals");
+			return "cereal";
+		}
+	//Honey Monster Foods
+		@GetMapping("/honeyM")
+		public String honeyM(Model model) {
+			model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Honey Monster Foods"));
+			model.addAttribute("pageTitle", "Honey Monster Foods' Cereals");
+			return "cereal";
+		}
+	//Quaker
+		@GetMapping("/quaker")
+		public String quaker(Model model) {
+			model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Quaker"));
+			model.addAttribute("pageTitle", "Quaker's Cereals");
+			return "cereal";
+		}
+		
+	//Tesco
+		@GetMapping("/tesco")
+		public String tesco(Model model) {
+			model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Tesco"));
+			model.addAttribute("pageTitle", "Tesco's Cereals");
+			return "cereal";
+		}
+	//Jordans
+		@GetMapping("/jordans")
+		public String jordans(Model model) {
+			model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Jordans"));
+			model.addAttribute("pageTitle", "Honey Monster Foods' Cereals");
+			return "cereal";
+		}
+	//Asda
+		@GetMapping("/asda")
+		public String asda(Model model) {
+			model.addAttribute("cereal", this.cerealRepository.findAllByManufacturer("Asda"));
+			model.addAttribute("pageTitle", "Honey Monster Foods' Cereals");
+			return "cereal";
+		}
 }
--- Cereal/src/main/java/uk/ac/belfastmet/cereal/repositories/CerealRepository.java
+++ Cereal/src/main/java/uk/ac/belfastmet/cereal/repositories/CerealRepository.java
@@ -7,4 +7,6 @@ import uk.ac.belfastmet.cereal.domain.Cereal;
 public interface CerealRepository extends CrudRepository<Cereal, Integer>{
 
 	Iterable<Cereal> findAllByCerealContaining(String cereal);
+
+	Iterable<Cereal> findAllByManufacturer(String string);
 }
