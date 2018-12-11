95081b53072d7c1d836c4d1ba0a73e6f3cb84765
MCE18161675 <mce18161675@belfastmet.ac.uk>
2018-12-11 14:56:28 +0000

    Update: Slowly routing the save button

--- 09-02-TitanicConsumer/src/main/java/uk/ac/belfastmet/consumer/controller/PassengerController.java
+++ 09-02-TitanicConsumer/src/main/java/uk/ac/belfastmet/consumer/controller/PassengerController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import uk.ac.belfastmet.consumer.domain.AllPassengers;
 import uk.ac.belfastmet.consumer.domain.Passenger;
@@ -19,23 +21,23 @@ public class PassengerController {
 	PassengerServiceImpl passengerService;
 	AllPassengers allPassengers;
 
-	
-	//Constuctors
+	// Constuctors
 	public PassengerController() {
 		super();
 	}
+
 	public PassengerController(PassengerServiceImpl passengerService, AllPassengers allPassengers) {
 		super();
 		this.passengerService = passengerService;
 		this.allPassengers = allPassengers;
 	}
+
 	public PassengerController(PassengerServiceImpl passengerService) {
 		super();
 		this.passengerService = passengerService;
 	}
 
-	
-	//Viewing All Passengers
+	// Viewing All Passengers
 	@GetMapping("/passengers")
 	public String allPassengers(Model model) {
 		ArrayList<Passenger> passenger = this.passengerService.list();
@@ -43,7 +45,8 @@ public class PassengerController {
 		model.addAttribute("passenger", passenger);
 		return "passengers";
 	}
-	//Viewing One Passenger
+
+	// Viewing One Passenger
 	@GetMapping("/{passengerId}")
 	public String viewPassenger(@PathVariable("passengerId") Integer passengerId, Model model) {
 		model.addAttribute("pageTitle", "View Passenger");
@@ -53,23 +56,35 @@ public class PassengerController {
 		return "passenger";
 
 	}
-	//Editing One passenger
+
+	// Editing One passenger
 	@GetMapping("passengers/edit/{passengerId}")
-	public String editPassenger(@PathVariable("passengerId") Integer passengerId, Model model)	{
+	public String editPassenger(@PathVariable("passengerId") Integer passengerId, Model model) {
 		Passenger passenger = this.passengerService.get(passengerId);
 		model.addAttribute("pageTitle", "Edit A Record");
 		model.addAttribute("passenger", this.passengerService.update(passenger));
-	return "editpassenger";	
+		return "editpassenger";
 	}
-	
-	
 
-	//@GetMapping("/add")
-//	public String addPassenger(Integer passengerId, Model model) {
-//		model.addAttribute("pageTitle", "Add Passenger");
-//		Passenger passenger = this.passengerService.add(passenger);
-//		model.addAttribute("passenger", passenger);
-//
-//	}
+	// Adding one Passenger
+	@GetMapping("/passengers/add/")
+	public String addPassenger(Model model) {
+		model.addAttribute("pageTitle", "Add Passenger");
+		
+		Passenger passenger = new Passenger();
+		model.addAttribute("passenger", passenger);
+		return "editpassenger";
+	}
 
+	@PostMapping("/passengers/save")
+	public String save(Passenger passenger, @RequestParam("addUpdate") String addUpdate, Model model) {
+	if(addUpdate.equals("Add Passenger")) {
+		this.passengerService.add(passenger);
+		return "redirect:/titanic";
+	}
+	else {
+		this.passengerService.update(passenger);
+		return "redirect:/titanic/view/" + passenger.getPassengerId();
+	}
+	}
 }
